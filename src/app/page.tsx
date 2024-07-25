1e48e7884abcf58566954d1bde5ba47e862f5177
Pharm-ack <75676193+Pharm-ack@users.noreply.github.com>
2024-07-25 04:01:03 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import LoginForm from "@/components/login-form";
+import Logo from "@/components/logo";
 export default function Home() {
   return (
-    <main className="flex justify-center items-center min-h-screen">
+    <main className="p-3 flex gap-y-8 flex-col items-center min-h-screen">
+      <div className="w-full sm:w-auto">
+        <Logo />
+      </div>
       <LoginForm />
     </main>
   );
